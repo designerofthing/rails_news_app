626c0c56bca7ac9eea9efacc2e3c4315c96176cd
designerofthing <designerofthings@gmail.com>
2020-04-20 22:20:14 +0200

    passes unit tests

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,4 +1,6 @@
-Rspec.describe Article, type: :model do
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
     describe 'DB table' do
         it { is_expected.to have_db_column :id }
         it { is_expected.to have_db_column :title }
