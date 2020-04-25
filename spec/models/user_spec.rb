a65e0689c08204a4969da80fb9c3a1f04bbee2f5
designerofthing <designerofthings@gmail.com>
2020-04-25 09:05:44 +0200

    adds user db table tests, vaildation tests and factory test to user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+feature 'check user db table' do
+  let(:user) { FactoryBot.create(:user, email: 'testing@gmail.com') }
+
+  describe User, type: :model do
+    describe 'user db table' do
+      it { is_expected.to have_db_column :email }
+      it { is_expected.to have_db_column :password }
+      it { is_expected.to have_db_column :encrypted_password }
+    end
+
+    describe 'Validations' do
+      it { is_expected.to validate_presence_of :email }
+      it { is_expected.to validate_presence_of :password }
+      it { is_expected.to validate_presence_of :encrypted_password }
+    end
+
+    describe 'Factory' do
+      it 'should have valid Factory' do
+        expect(user).to be_valid
+      end
+    end
+  end
 end
