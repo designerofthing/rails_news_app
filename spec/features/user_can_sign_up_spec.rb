d7532cff50dbea7db9179c8ad829b7224170fbcd
designerofthing <designerofthings@gmail.com>
2020-04-25 08:58:47 +0200

    adds test to input email and password to sign up, passes test

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -11,4 +11,12 @@ feature 'user can sign up' do
   it 'user should be on the sign up page' do
       expect(page).to have_content 'Sign up'
   end
+
+  it 'user can input email and password to sign up' do
+    fill_in 'Email', with: 'test@gmail.com'
+    fill_in 'Password', with: '123456'
+    fill_in 'Password confirmation', with: '123456'
+    click_on 'Sign up'
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+  end
 end
