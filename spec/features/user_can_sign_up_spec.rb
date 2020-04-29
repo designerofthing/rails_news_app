d0e971d3f121827b12c0a4e40f9afafc30bb2ab7
designerofthing <designerofthings@gmail.com>
2020-04-29 17:11:33 +0200

    corrects code to meet comments in PR

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-feature 'user can sign up' do
+feature 'user can sign up (happy path)' do
   before do
     visit root_path
     click_on 'Sign Up'
@@ -18,5 +18,28 @@ feature 'user can sign up' do
     fill_in 'Password confirmation', with: '123456'
     click_on 'Sign up'
     expect(page).to have_content 'Welcome! You have signed up successfully.'
+  end
+end
+
+feature 'user can sign up (sad path)' do
+  before do
+    visit root_path
+    click_on 'Sign Up'
+  end
+
+  it 'user fails to input password twice correctly' do
+    fill_in 'Email', with: 'test@gmail.com'
+    fill_in 'Password', with: '123456'
+    fill_in 'Password confirmation', with: '123457'
+    click_on 'Sign up'
+    expect(page).to have_content 'error'
+  end
+
+  it 'user fails to input email correctly' do
+    fill_in 'Email', with: 'testgmail.com'
+    fill_in 'Password', with: '123456'
+    fill_in 'Password confirmation', with: '123456'
+    click_on 'Sign up'
+    expect(page).to have_content 'error'
   end
 end
